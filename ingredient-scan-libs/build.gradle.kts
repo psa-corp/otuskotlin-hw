ec27dfa88cb858af12b05af0ec84fa42c1d577c0
psa-corp <samorodovcorp@gmail.com>
2025-11-28 22:49:56 +0700

    m4l1 set spec-log1 to libs

--- ingredient-scan-libs/build.gradle.kts
+++ ingredient-scan-libs/build.gradle.kts
@@ -22,6 +22,7 @@ ext {
     val specDir = layout.projectDirectory.dir("../specs")
     set("spec-v1-external", specDir.file("specs-external-api-v1.yaml").toString())
     set("spec-v1-internal", specDir.file("specs-internal-api-v1.yaml").toString())
+    set("spec-log1", specDir.file("specs-scan-log1.yaml").toString())
 }
 
 tasks {
