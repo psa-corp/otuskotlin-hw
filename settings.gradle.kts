8216e28708834a596ad892a01e096e7f1dedd180
psa-corp <samorodovcorp@gmail.com>
2025-12-25 02:13:33 +0700

    m5l1 (prepare hw) skip ingredient-scan-libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,5 +8,5 @@ rootProject.name = "otuskotlin"
 //includeBuild("lessons")
 includeBuild("plugins")
 includeBuild("ingredient-scan")
-includeBuild("ingredient-scan-libs")
+//includeBuild("ingredient-scan-libs")
 includeBuild("ingredient-scan-tests")
