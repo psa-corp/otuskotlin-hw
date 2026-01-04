7da25e597c7709a2af947e2a105847afcb91e8f7
Samorodov Pavel <paul.g@amzscout.net>
2026-01-05 02:00:09 +0700

    m5l1 create biz common

---
+++ ingredient-scan/biz/biz-common/src/main/kotlin/net/otuskotlin/ingredientscan/biz/common/IsBizProcessor.kt
@@ -0,0 +1,25 @@
+package net.otuskotlin.ingredientscan.biz.common
+
+import net.otuskotlin.ingredientscan.core.common.external.IsContext
+import net.otuskotlin.ingredientscan.core.common.external.IsCorSettings
+import net.otuskotlin.ingredientscan.core.common.external.models.IsCommand
+import net.otuskotlin.ingredientscan.core.common.external.models.IsState
+import net.otuskotlin.ingredientscan.core.common.external.stubs.IsAnalysisStub.Companion.STUB_ANALYSIS
+import net.otuskotlin.ingredientscan.core.common.external.stubs.IsCompositionStub.Companion.STUB_COMPOSITION
+import net.otuskotlin.ingredientscan.core.common.external.stubs.IsCompositionStub.Companion.STUB_COMPOSITION_CONTEXT_FINISHING
+
+
+class IsBizProcessor(settings: IsCorSettings) {
+    suspend fun exec(context: IsContext) {
+        when (context.command){
+            IsCommand.ANALYSIS_GET,
+            IsCommand.ANALYSIS_REGENERATE -> context.analysisResponse = STUB_ANALYSIS
+            IsCommand.COMPOSITION_GET -> context.compositionResponse = STUB_COMPOSITION
+            IsCommand.COMPOSITION_CONTEXT_GET,
+            IsCommand.COMPOSITION_CREATE_MANUAL,
+            IsCommand.COMPOSITION_CREATE_PHOTOS -> context.compositionContextResponse = STUB_COMPOSITION_CONTEXT_FINISHING
+            else -> {}
+        }
+        context.state = IsState.FINISHING
+    }
+}
