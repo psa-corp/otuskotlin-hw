62050d8113fbad60815ab82c4337172ef95ab82c
Samorodov Pavel <paul.g@amzscout.net>
2026-01-07 07:10:42 +0700

    m5l1 fixe smoke test

--- ingredient-scan/scanner/src/test/kotlin/net/otuskotlin/ingredientscan/scanner/ScannerApplicationTests.kt
+++ ingredient-scan/scanner/src/test/kotlin/net/otuskotlin/ingredientscan/scanner/ScannerApplicationTests.kt
@@ -3,6 +3,8 @@ package net.otuskotlin.ingredientscan.scanner
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import software.amazon.awssdk.services.s3.S3AsyncClient
 
 @SpringBootTest(
     properties = [
@@ -12,6 +14,8 @@ import org.springframework.test.context.ActiveProfiles
 )
 @ActiveProfiles("test")
 class ScannerApplicationTests {
+    @MockitoBean
+    lateinit var s3AsyncClient: S3AsyncClient
 
     @Test
     fun `context loads`() {
