6b2e236ffb4c4e691a49cea2d47203a8c7a55d05
Samorodov Pavel <paul.g@amzscout.net>
2026-01-07 07:13:47 +0700

    m5l1 media unit test is disabled. I'm going to test it in integration tests

--- ingredient-scan/scanner/src/test/kotlin/net/otuskotlin/ingredientscan/scanner/controllers/MediaControllerTest.kt
+++ ingredient-scan/scanner/src/test/kotlin/net/otuskotlin/ingredientscan/scanner/controllers/MediaControllerTest.kt
@@ -1,68 +1,79 @@
-//package net.otuskotlin.ingredientscan.scanner.controllers
-//
-//import kotlinx.coroutines.test.runTest
-//import net.otuskotlin.ingredientscan.api.v1.external.models.CompositionCreateByPhotosRequest
-//import net.otuskotlin.ingredientscan.api.v1.external.models.ScanPhotosDto
-//import net.otuskotlin.ingredientscan.api.v1.external.models.ScanType
-//import net.otuskotlin.ingredientscan.core.common.external.IsContext
-//import net.otuskotlin.ingredientscan.scanner.services.biz.BizService
-//import net.otuskotlin.ingredientscan.scanner.services.s3.S3CloudService
-//import net.otuskotlin.ingredientscan.scanner.utils.ControllerUtil.Companion.serializeRequest
-//import org.junit.jupiter.api.Test
-//import org.mockito.kotlin.any
-//import org.mockito.kotlin.whenever
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import org.springframework.http.MediaType
-//import org.springframework.mock.web.MockMultipartFile
-//import org.springframework.test.context.bean.override.mockito.MockitoBean
-//import org.springframework.test.web.servlet.MockMvc
-//import org.springframework.test.web.servlet.multipart
-//
-//@WebMvcTest(MediaController::class)
-//class MediaControllerTest {
-//
-//    @Autowired
-//    private lateinit var mockMvc: MockMvc
-//
-//    @MockitoBean
-//    private lateinit var s3CloudService: S3CloudService
-//
-//    @MockitoBean
-//    private lateinit var bizService: BizService
-//
+package net.otuskotlin.ingredientscan.scanner.controllers
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import net.otuskotlin.ingredientscan.api.v1.external.models.*
+import net.otuskotlin.ingredientscan.scanner.services.biz.BizService
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.http.MediaType
+import org.springframework.http.client.MultipartBodyBuilder
+import org.springframework.http.codec.multipart.FilePart
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.web.reactive.function.BodyInserters
+import reactor.core.publisher.Flux
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.Mockito.doAnswer
+import org.mockito.kotlin.any
+
+@WebFluxTest(MediaController::class)
+class MediaControllerTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @MockitoBean
+    private lateinit var bizService: BizService
+
+    //конфликт корутин и webflugs тест буду проверять на интеграционных тестах
+
 //    @Test
-//    fun `compositionCreateByPhotos returns successful response`() {
-//        // Arrange
+//    fun `compositionCreateByPhotos returns successful response`(): Unit = runBlocking {
+//    // Arrange
 //        val request = CompositionCreateByPhotosRequest(
 //            requestType = "compositionCreateByPhotos",
 //            scan = ScanPhotosDto(type = ScanType.PHOTO)
 //        )
-//        val scanDataPart = serializeRequest(request)
-//        val scanPart = createJsonMultipartFile("scan", "scan.json", scanDataPart)
-//        val photo1 = createImageMultipartFile("photos", "photo1.jpg", "photo1 data")
-//        val photo2 = createImageMultipartFile("photos", "photo2.jpg", "photo2 data")
 //
-//        whenever(s3CloudService.uploadFiles(any(), any(), any()))
-//            .thenReturn(mutableListOf("photo1.jpg", "photo2.jpg"))
-//        whenever(bizService.compositionCreateByPhotos(any()))
-//            .thenReturn(IsContext())
+//        val response = CompositionCreateByPhotosResponse(
+//            responseType = "compositionCreateByPhotos",
+//            result = ResponseResult.SUCCESS,
+//            contextId = "context_5678"
+//        )
+//
+//        doAnswer { response }
+//            .`when`(bizService)
+//            .execute(
+//                any<CompositionCreateByPhotosRequest>(),
+//                any()
+//            )
+//
+//        val builder = MultipartBodyBuilder().apply {
+//            part("photos", "photo1 data".toByteArray())
+//                .filename("photo1.jpg")
+//                .contentType(MediaType.IMAGE_JPEG)
+//
+//            part("scan",  request)
+//                .contentType(MediaType.APPLICATION_JSON)
+//        }
 //
 //        // Act & Assert
-//        mockMvc.multipart("/media/composition/create/photos") {
-//            file(photo1)
-//            file(photo2)
-//            file(scanPart)
-//        }.andExpect {
-//            status { isOk() }
-//        }
+//        webTestClient.post()
+//            .uri("/media/composition/create/photos")
+//            .contentType(MediaType.MULTIPART_FORM_DATA)
+//            .body(BodyInserters.fromMultipartData(builder.build()))
+//            .exchange()
+//            .expectStatus().isOk
+//            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+//            .expectBody<CompositionCreateByPhotosResponse>()
+//            .value {
+//                assert(it.result == ResponseResult.SUCCESS)
+//                assert(it.contextId == "context_5678")
+//            }
 //    }
-//
-//    private fun createJsonMultipartFile(name: String, filename: String, content: String): MockMultipartFile {
-//        return MockMultipartFile(name, filename, MediaType.APPLICATION_JSON_VALUE, content.toByteArray())
-//    }
-//
-//    private fun createImageMultipartFile(name: String, filename: String, data: String): MockMultipartFile {
-//        return MockMultipartFile(name, filename, "image/jpeg", data.toByteArray())
-//    }
-//}
+}
