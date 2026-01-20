6b26dd71870dd8dd5801d3c4fade96d9bc69c371
Samorodov Pavel <paul.g@amzscout.net>
2026-01-21 02:18:41 +0700

    m6l1_m7l1 add submit helper to connect biz

---
+++ ingredient-scan/app/app-common/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("plugin-jvm")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(libs.jackson.kotlin)
+    implementation(libs.jackson.datatype)
+
+    implementation(projects.api.apiLog1)
+    implementation(projects.api.apiV1ExternalJackson)
+    implementation(projects.api.apiV1ExternalMappers)
+    implementation(projects.core.coreCommon)
+    implementation(projects.core.coreStubs)
+    implementation(projects.biz.bizCommon)
+
+    implementation(libs.coroutines.reactor)
+    testImplementation(libs.coroutines.test)
+    testImplementation(kotlin("test-junit"))
+}
